c1d4783d035acc19bd3ef335b1f2a9063d2ffd89
but212 <wjddlf211@naver.com>
2025-11-13 15:52:21 +0900

    [feat] 폰트 설정 pretendard variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import Provider from '@/app/Provider'
 import '@/styles/global.css'
+import fonts from '@/fonts'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko-KR">
-      <body>
+      <body className={fonts.pretendard.className}>
         <Provider>{children}</Provider>
       </body>
     </html>
