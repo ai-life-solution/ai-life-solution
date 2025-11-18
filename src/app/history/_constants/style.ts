63f5ce345398867be1ee4deceb4bacf9080b0d9d
but212 <wjddlf211@naver.com>
2025-11-18 14:45:21 +0900

    [refactor] 히스토리 페이지 min-h 계산식 조정
    - PAGE.CONTENTS min-h 136px → 120px로 변경
    - PAGE.MAIN에 min-h-[calc(100vh_-_136px)] 추가

--- src/app/history/_constants/style.ts
+++ src/app/history/_constants/style.ts
@@ -2,13 +2,14 @@ import { cn } from '@/utils'
 
 export const PAGE = {
   CONTAINER: cn('bg-(--color-etc-2)', 'w-full', 'min-h-screen'),
-  CONTENTS: cn('p-8', 'bg-gray-100', 'rounded-t-3xl', 'min-h-[calc(100vh_-_136px)]'),
+  CONTENTS: cn('p-8', 'bg-gray-100', 'rounded-t-3xl', 'min-h-[calc(100vh_-_120px)]'),
   MAIN: cn(
     'flex',
     'flex-col',
     'items-center',
     'w-full',
     'h-full',
+    'min-h-[calc(100vh_-_136px)]'
   ),
 } as const
 
