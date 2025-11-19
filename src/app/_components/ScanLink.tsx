89d8e94e277045e7ada36009ad88305891cc2bb8
WUHYERI <woohree0125@gmail.com>
2025-11-20 03:59:14 +0900

    [chore] 스캔 시작 링크에 '/scan' 경로 연결

--- src/app/_components/ScanLink.tsx
+++ src/app/_components/ScanLink.tsx
@@ -6,7 +6,7 @@ import { SCAN_LINK_CLASS } from '../_constants/style'
 
 export default function ScanLink() {
   return (
-    <Link href="/" className={SCAN_LINK_CLASS}>
+    <Link href="/scan" className={SCAN_LINK_CLASS}>
       스캔시작
       <Camera size={24} />
     </Link>
