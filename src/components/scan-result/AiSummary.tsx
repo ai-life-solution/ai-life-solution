3e3bdf58df3e5d9251539a39410e83283ac03de2
WUHYERI <woohree0125@gmail.com>
2025-11-28 16:54:50 +0900

    [feat] AI 요약 로딩상태 추가
    - Spinner.tsx 컴포넌트 추가
    - AI 요약 로딩상태 UI 반영

--- src/components/scan-result/AiSummary.tsx
+++ src/components/scan-result/AiSummary.tsx
@@ -1,5 +1,7 @@
 import type { FoodItem } from '@/types/FoodItem'
 
+import Spinner from '../Spinner'
+
 import { AI_SUMMARY_CONTAINER_CLASS } from './_constants/style'
 
 interface Props {
@@ -8,9 +10,11 @@ interface Props {
 
 export default function AiSummary({ data }: Props) {
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col w-full gap-5">
       <p className="font-bold text-xl">AI의 요약 정보</p>
-      <div className={AI_SUMMARY_CONTAINER_CLASS}>{data.description}</div>
+      <div className={AI_SUMMARY_CONTAINER_CLASS}>
+        {data.description ? data.description : <Spinner message="AI 요약을 생성중입니다." />}
+      </div>
     </div>
   )
 }
